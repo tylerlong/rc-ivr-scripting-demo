54e0a3895d7e7f999e37f8b48f66b3ac9d2e25b4
Tyler Long <tyler4long@gmail.com>
2019-12-09 14:41:33 -0800

    Print log

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const express = require('express')
 const serverlessHTTP = require('serverless-http')
 
 const app = express()
-app.get('/hello', (req, res) => {
-  res.send('world')
-})
 
 app.post('/on-call-enter', (req, res) => {
-  res.send('on-call-enter')
+  console.log('/on-call-enter')
+  res.send('/on-call-enter')
 })
 
 app.post('/on-call-exit', (req, res) => {
-
+  console.log('/on-call-exit')
+  res.send('/on-call-exit')
 })
 
 app.post('/on-command-update', (req, res) => {
-
+  console.log('/on-command-update')
+  res.send('/on-command-update')
 })
 
 app.post('/on-command-error', (req, res) => {
-
+  console.log('/on-command-error')
+  res.send('/on-command-error')
 })
 
 module.exports.app = serverlessHTTP(app)
