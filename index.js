140f291309de27bacf621857e687aac0320b4bb5
Tyler Long <tyler4long@gmail.com>
2019-12-10 11:45:08 -0800

    Create lambda proxy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const serverlessHTTP = require('serverless-http')
 const RingCentral = require('ringcentral-js-concise').default
+const Lambda = require('aws-sdk/clients/lambda')
 
 const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
 rc.token({
@@ -13,7 +14,7 @@ app.use(express.json())
 app.post('/on-call-enter', async (req, res) => {
   console.log('/on-call-enter')
   const { partyId, sessionId } = req.body
-  res.status(204).send()
+  // res.status(204).send()
   await rc.post(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}/parties/${partyId}/play`, {
     resources: [
       {
@@ -27,17 +28,41 @@ app.post('/on-call-enter', async (req, res) => {
 
 app.post('/on-call-exit', (req, res) => {
   console.log('/on-call-exit')
-  res.status(204).send()
+  // res.status(204).send()
 })
 
 app.post('/on-command-update', (req, res) => {
   console.log('/on-command-update')
-  res.status(204).send()
+  // res.status(204).send()
 })
 
 app.post('/on-command-error', (req, res) => {
   console.log('/on-command-error')
-  res.status(204).send()
+  // res.status(204).send()
 })
 
 module.exports.app = serverlessHTTP(app)
+
+const createAsyncProxy = functionName => {
+  const lambda = new Lambda({ region: process.env.AWS_REGION })
+  return async (event, context) => {
+    const lambdaFunction = async () => {
+      await lambda.invoke({
+        FunctionName: process.env.AWS_LAMBDA_FUNCTION_NAME.replace(/-proxy$/, `-${functionName}`),
+        InvocationType: 'Event',
+        Payload: JSON.stringify(event)
+      }).promise()
+      return {
+        statusCode: 204,
+        headers: {
+          'Validation-Token': event.headers['Validation-Token'],
+          'Content-Type': 'text/html'
+        },
+        body: '<!doctype><html><body><script>close()</script><p>Please close this page</p></body></html>'
+      }
+    }
+    return lambdaFunction()
+  }
+}
+
+module.exports.proxy = createAsyncProxy('app')
