b00b5758691b4265f2bddb74ca9e4f42ac42a3c6
Tyler Long <tyler4long@gmail.com>
2019-12-10 15:47:26 -0800

    Refactor code

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const RingCentral = require('ringcentral-js-concise').default
+
+const app = express()
+app.use(express.json())
+
+const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
+rc.token({
+  access_token: process.env.RINGCENTRAL_TOKEN
+})
+
+app.post('/on-call-enter', async (req, res) => {
+  console.log('/on-call-enter')
+  res.status(204).send()
+  const { partyId, sessionId } = req.body
+  try {
+    await rc.post(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}/parties/${partyId}/play`, {
+      resources: [
+        {
+          uri: 'http://chuntaoliu.com/rc-ivr-scripting-demo/greetings.wav'
+        }
+      ],
+      interruptByDtmf: false,
+      repeatCount: 1
+    })
+  } catch (e) {
+    console.log(e.message.replace(/[\r\n]+/g, '\t'))
+    try {
+      const r1 = await rc.get(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}`)
+      console.log(`session ${sessionId} exists: ${JSON.stringify(r1.data)}`)
+    } catch (e) {
+      console.log(`session ${sessionId} returns ${e.status} ${e.statusText}`)
+    }
+    try {
+      const r2 = await rc.get(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}/parties/${partyId}`)
+      console.log(`party ${partyId} exists: : ${JSON.stringify(r2.data)}`)
+    } catch (e) {
+      console.log(`party ${partyId} returns ${e.status} ${e.statusText}`)
+    }
+  }
+})
+
+app.post('/on-call-exit', (req, res) => {
+  console.log('/on-call-exit')
+  // res.status(204).send()
+})
+
+app.post('/on-command-update', (req, res) => {
+  console.log('/on-command-update')
+  // res.status(204).send()
+})
+
+app.post('/on-command-error', (req, res) => {
+  console.log('/on-command-error')
+  // res.status(204).send()
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,61 +1,8 @@
-const express = require('express')
+// const express = require('express')
 const serverlessHTTP = require('serverless-http')
-const RingCentral = require('ringcentral-js-concise').default
 const Lambda = require('aws-sdk/clients/lambda')
 
-const rc = new RingCentral('', '', process.env.RINGCENTRAL_SERVER_URL)
-rc.token({
-  access_token: process.env.RINGCENTRAL_TOKEN
-})
-
-const app = express()
-app.use(express.json())
-
-app.post('/on-call-enter', async (req, res) => {
-  console.log('/on-call-enter')
-  const { partyId, sessionId } = req.body
-  try {
-    await rc.post(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}/parties/${partyId}/play`, {
-      resources: [
-        {
-          uri: 'http://chuntaoliu.com/rc-ivr-scripting-demo/greetings.wav'
-        }
-      ],
-      interruptByDtmf: false,
-      repeatCount: 1
-    })
-  } catch (e) {
-    console.log(e.message.replace(/[\r\n]+/g, '\t'))
-    try {
-      const r1 = await rc.get(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}`)
-      console.log(`session ${sessionId} exists: ${JSON.stringify(r1.data)}`)
-    } catch (e) {
-      console.log(`session ${sessionId} returns ${e.status} ${e.statusText}`)
-    }
-    try {
-      const r2 = await rc.get(`/restapi/v1.0/account/~/telephony/sessions/${sessionId}/parties/${partyId}`)
-      console.log(`party ${partyId} exists: : ${JSON.stringify(r2.data)}`)
-    } catch (e) {
-      console.log(`party ${partyId} returns ${e.status} ${e.statusText}`)
-    }
-  }
-})
-
-app.post('/on-call-exit', (req, res) => {
-  console.log('/on-call-exit')
-  // res.status(204).send()
-})
-
-app.post('/on-command-update', (req, res) => {
-  console.log('/on-command-update')
-  // res.status(204).send()
-})
-
-app.post('/on-command-error', (req, res) => {
-  console.log('/on-command-error')
-  // res.status(204).send()
-})
-
+const app = require('./app')
 module.exports.app = serverlessHTTP(app)
 
 const createAsyncProxy = functionName => {
@@ -67,13 +14,14 @@ const createAsyncProxy = functionName => {
         InvocationType: 'Event',
         Payload: JSON.stringify(event)
       }).promise()
+      console.log('return to IVR server side')
       return {
-        statusCode: 204,
-        headers: {
-          'Validation-Token': event.headers['Validation-Token'],
-          'Content-Type': 'text/html'
-        },
-        body: '<!doctype><html><body><script>close()</script><p>Please close this page</p></body></html>'
+        statusCode: 204
+        // headers: {
+        //   'Validation-Token': event.headers['Validation-Token'],
+        //   'Content-Type': 'text/html'
+        // },
+        // body: '<!doctype><html><body><script>close()</script><p>Please close this page</p></body></html>'
       }
     }
     console.log('proxy hit')
